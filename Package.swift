0c5efdf40c8cd323b640f65ea580b93cbc1dcb75
Robert F. Dickerson <rfdickerson@gmail.com>
2016-07-08 15:46:47 -0500

    Changed Package name- added PostgreSQL dep

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "todolist-boilerplate",
+    name: "TodoList",
     dependencies: [
          .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 19),
          .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 9),
          .Package(url: "https://github.com/IBM-Swift/todolist-web", majorVersion: 0),
          .Package(url: "https://github.com/IBM-Swift/todolist-api", majorVersion: 0),
+         .Package(url: "https://github.com/Zewo/PostgreSQL", majorVersion: 0)
     ]
 )
