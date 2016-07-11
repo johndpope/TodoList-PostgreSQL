66b5a2b7ad440e56b80061d5320740bbaf99ad3f
Chia Huang <chialh@uci.edu>
2016-07-11 14:15:30 -0500

    Added init for local and dbconfiguration

--- Sources/TodoList.swift
+++ Sources/TodoList.swift
@@ -36,16 +36,59 @@ import PostgreSQL
 public final class TodoList : TodoListAPI {
     
     static let defaultPostgreHost = "localhost"
-    static let defaultPort = 5432
+    static let defaultPostgrePort = Int32(5432)
     static let defaultDatabaseName = "todolist"
-    static let defaultUsername = ""
+    static let defaultUsername = "chiahuang"
     static let defaultPassword = ""
+//    let postgreConnection: PostgreSQL.Connection!
+    
+    var host: String = TodoList.defaultPostgreHost
+    var port: Int32 = TodoList.defaultPostgrePort
+    var password: String = TodoList.defaultPassword
+    var username: String = TodoList.defaultUsername
+    var database : String = TodoList.defaultDatabaseName
     
     //let connectionString = "postgres://localhost:5432/todolist"
     
+    public init(database: String = TodoList.defaultDatabaseName,
+                host: String = TodoList.defaultPostgreHost,
+                port: Int32 = TodoList.defaultPostgrePort,
+                username: String? = defaultUsername, password: String? = defaultPassword) {
+        
+//        do {
+            self.database = database
+            self.host = host
+            self.port = Int32(port)
+            self.username = username!
+            self.password = password!
+            
+//            self.postgreConnection = try PostgreSQL.Connection(URI("postgres://\(username):\(password)@\(host):\(port)/\(database)"))
+        
+//        } catch {
+//            Log.error("Postgre is not available on host: \(host) and port: \(port)")
+//            print("Failed to connect in local")
+//        }
+    }
+    
+    public init(_ dbConfiguration: DatabaseConfiguration) {
+        
+//        do {
+        self.host = dbConfiguration.host!
+        self.port = Int32(dbConfiguration.port!)
+        self.username = dbConfiguration.username!
+        self.password = dbConfiguration.password!
+        
+//        self.postgreConnection = try PostgreSQL.Connection(URI("postgres://\(username):\(password)@\(host):\(port)/\(database)"))
+//        } catch {
+//            Log.error("Postgre is not available on host: \(host) and port: \(port)")
+//            print("Failed to connect in configuration")
+
+//        }
+    }
+    
     public func count(withUserID userID: String?, oncompletion: (Int?, ErrorProtocol?) -> Void) {
     
-        let query = "SELECT COUNT(*) FROM todos WHERE owner_id=\(userID)"
+//        let query = "SELECT COUNT(*) FROM todos WHERE owner_id=\(userID)"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -61,7 +104,7 @@ public final class TodoList : TodoListAPI {
     
     public func clear(withUserID ownerID: String?, oncompletion: (ErrorProtocol?) -> Void) {
         
-        let query = "DELETE FROM todos WHERE owner=\(ownerID)"
+//        let query = "DELETE FROM todos WHERE owner=\(ownerID)"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -76,7 +119,7 @@ public final class TodoList : TodoListAPI {
     
     public func clearAll(oncompletion: (ErrorProtocol?) -> Void) {
         
-        let query = "TRUNCATE TABLE todos"
+//        let query = "TRUNCATE TABLE todos"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -91,7 +134,7 @@ public final class TodoList : TodoListAPI {
     
     public func get(withUserID userID: String?, oncompletion: ([TodoItem]?, ErrorProtocol?) -> Void) {
         
-        let query = "SELECT * FROM todos WHERE owner_id=\(userID)"
+//        let query = "SELECT * FROM todos WHERE owner_id=\(userID)"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -107,7 +150,7 @@ public final class TodoList : TodoListAPI {
     
     public func get(withUserID userID: String?, withDocumentID documentID: String, oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
         
-        let query = "SELECT * FROM todos WHERE ownerid=\(userID) AND tid=\(documentID)"
+//        let query = "SELECT * FROM todos WHERE ownerid=\(userID) AND tid=\(documentID)"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -124,7 +167,7 @@ public final class TodoList : TodoListAPI {
     public func add(userID: String?, title: String, order: Int, completed: Bool,
              oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
         
-        let query = "INSERT INTO todos (title, owner_id, completed, orderno) VALUES (\(title), \(userID), \(completed), \(order));"
+//        let query = "INSERT INTO todos (title, owner_id, completed, orderno) VALUES (\(title), \(userID), \(completed), \(order));"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -141,7 +184,7 @@ public final class TodoList : TodoListAPI {
     public func update(documentID: String, userID: String?, title: String?, order: Int?,
                 completed: Bool?, oncompletion: (TodoItem?, ErrorProtocol?) -> Void ) {
         
-        let query = "UPDATE todos SET completed=\(completed) WHERE tid=\(documentID)"
+//        let query = "UPDATE todos SET completed=\(completed) WHERE tid=\(documentID)"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
@@ -157,7 +200,7 @@ public final class TodoList : TodoListAPI {
     
     public func delete(withUserID userID: String?, withDocumentID documentID: String, oncompletion: (ErrorProtocol?) -> Void) {
     
-        let query = "DELETE FROM todos WHERE owner_id=\(userID) AND tid=\(documentID)"
+//        let query = "DELETE FROM todos WHERE owner_id=\(userID) AND tid=\(documentID)"
         
 //        do {
 //            let connection = try Connection(URI(connectionString))
