db35f054607cc416a79223d670ff9c6b2ae3b034
Jessica Pittman <jpittma@us.ibm.com>
2016-10-17 18:14:28 -0500

    Added swift-cfenv to package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     dependencies: [
          .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
          .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
-         .Package(url: "https://github.com/Zewo/PostgreSQL", majorVersion: 0, minor: 14)
+         .Package(url: "https://github.com/Zewo/PostgreSQL", majorVersion: 0, minor: 14),
+         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git",   majorVersion: 1, minor: 7)
     ]
 )
